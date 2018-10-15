d6e088af0c8e85e8381efcdaec94a436f8284542
iAmGrant <claunch.grant@gmail.com>
2018-10-15 00:13:10 -0500

    Update styles

--- additem.js
+++ additem.js
@@ -30,15 +30,15 @@ list.addEventListener('click', function(ev) {
 // Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
+  var inputValue = document.getElementById("addItem").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
     alert("You must write something!");
   } else {
-    document.getElementById("myUL").appendChild(li);
+    document.getElementById("main-list").appendChild(li);
   }
-  document.getElementById("myInput").value = "";
+  document.getElementById("addItem").value = "";
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
